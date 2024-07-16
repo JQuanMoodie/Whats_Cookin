3b6101889df7d5807ebd42f0b55feabbf3f15e86
JevonW <106031224+jevonwil124@users.noreply.github.com>
2024-07-16 15:20:51 -0400

    Update ViewModel.swift

--- ViewModel.swift
+++ ViewModel.swift
@@ -5,58 +5,36 @@
 //  Created by Jevon Williams on 7/10/24.
 //
 
-import PhotosUI
-import SwiftUI
-class ProfileViewModel: ObservableObject{
-    @Published var profileImage: Image? = nil
-        @Published var selectedItem: PhotosPickerItem? {
-            didSet {
-                loadPhoto()
-            }
+import UIKit
+
+class ProfileViewModel {
+
+    @Published var profileImage: UIImage? {
+        didSet {
+            saveProfileImage(image: profileImage)
         }
-        
-        private let profileImageKey = "profileImageData"
-        
-        init() {
-            loadProfileImage()
+    }
+
+    private let profileImageKey = "profileImageData"
+
+    init() {
+        loadProfileImage()
+    }
+
+    func loadProfileImage() {
+        if let data = UserDefaults.standard.data(forKey: profileImageKey),
+           let uiImage = UIImage(data: data) {
+            profileImage = uiImage
         }
-        
-        func loadPhoto() {
-            Task {
-                // Retrieve selected item
-                guard let selectedItem = selectedItem else { return }
-                
-                // Retrieve selected item’s data
-                if let data = try? await selectedItem.loadTransferable(type: Data.self) {
-                    if let uiImage = UIImage(data: data) {
-                        profileImage = Image(uiImage: uiImage)
-                        
-                        // Save profile image to UserDefaults
-                        UserDefaults.standard.set(data, forKey: profileImageKey)
-                    }
-                }
-            }
+    }
+
+    func saveProfileImage(image: UIImage?) {
+        if let image = image, let imageData = image.jpegData(compressionQuality: 1.0) {
+            UserDefaults.standard.set(imageData, forKey: profileImageKey)
+        } else {
+            UserDefaults.standard.removeObject(forKey: profileImageKey)
         }
-        
-        func loadProfileImage() {
-            if let data = UserDefaults.standard.data(forKey: profileImageKey),
-               let uiImage = UIImage(data: data) {
-                profileImage = Image(uiImage: uiImage)
-            }
-        }
-    //@Published var selectedItem: PhotosPickerItem?{
-      //  didSet{Task{try await loadImage()}}
-    //}
-    //@Published var profileImage: Image?
-    
-    //func loadImage() async throws{
-      //  guard let item = selectedItem else{return}
-       // guard let imageData = try await item.loadTransferable(type: Data.self)else{return}
-      //  guard let uiImage = UIImage(data:imageData)else{return}
-      //  self.profileImage = Image(uiImage:uiImage)
-        
-    //}
-    
+    }
 }
     
    
