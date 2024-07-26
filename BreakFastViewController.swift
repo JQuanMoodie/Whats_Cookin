0672bb2cc8ba4ce67bd4bee804fcab57aadf5454
Jose Vasquez <77944957+Mamasuchi@users.noreply.github.com>
2024-07-26 19:03:35 -0400

    Create BreakFastViewController.swift

---
+++ BreakFastViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BreakFastViewController.swift
+//  capstoneproj
+//
+//  Created by Jose Vasquez on 7/26/24.
+//
+
+import UIKit
+
+class BreakfastViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Breakfast"
+        // Additional setup here
+    }
+}
