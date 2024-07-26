cf787945005e2e5cc84330ed3c081fc258a2db0e
Jose Vasquez <77944957+Mamasuchi@users.noreply.github.com>
2024-07-26 19:08:13 -0400

    Create DinnerViewController.swift

---
+++ DinnerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DinnerViewController.swift
+//  capstoneproj
+//
+//  Created by Jose on 7/26/24.
+//
+
+import UIKit
+
+class DinnerViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Dinner"
+        // Additional setup here
+    }
+}
