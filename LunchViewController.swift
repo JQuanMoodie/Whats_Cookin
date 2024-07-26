a902e05fbfef384df5ddbadc10aae1a69a1545cb
Jose Vasquez <77944957+Mamasuchi@users.noreply.github.com>
2024-07-26 19:07:12 -0400

    Create LunchViewController.swift

---
+++ LunchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LunchViewController.swift
+//  capstoneproj
+//
+//  Created by Jose on 7/26/24.
+//
+
+import UIKit
+
+class LunchViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Lunch"
+        // Additional setup here
+    }
+}
