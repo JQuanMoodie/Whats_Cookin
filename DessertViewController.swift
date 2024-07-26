c15868cee27124f5b47e74fa7f4723908601a55b
Jose Vasquez <77944957+Mamasuchi@users.noreply.github.com>
2024-07-26 19:09:22 -0400

    Create DessertViewController.swift

---
+++ DessertViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DessertViewController.swift
+//  capstoneproj
+//
+//  Created by Jose on 7/26/24.
+//
+
+import UIKit
+
+class DessertViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Dessert"
+        // Additional setup here
+    }
+}
