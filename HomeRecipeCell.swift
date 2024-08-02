39a0a001578e7437460523b799f67dc7827d2ecb
Raisa Methila <92753269+raisax2@users.noreply.github.com>
2024-08-01 22:54:06 -0400

    Add files via upload

---
+++ HomeRecipeCell.swift
@@ -0,0 +1,61 @@
+//
+//  HomeRecipeCell.swift
+//  What'sCookin
+//
+//  Created by Raisa Methila on 8/1/24.
+//
+
+import UIKit
+
+class RecipeCollectionViewCell: UICollectionViewCell {
+    static let identifier = "RecipeCollectionViewCell"
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        contentView.addSubview(titleLabel)
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.heightAnchor.constraint(equalTo: contentView.widthAnchor), // Make image view square
+            
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+    func configure(with recipe: Recipee) {
+        titleLabel.text = recipe.title
+        if let imageUrl = recipe.imageURL {
+            imageView.load(url: imageUrl)
+        }
+    }
+}
+
